cd37042d637d4bea248f0dad28199cdcaf5fad04
Dhanyashree <dhanyashreem2400@gmail.com>
2023-08-29 11:42:44 +0530

    CODE

--- user_funnel_analysis.py
+++ user_funnel_analysis.py
@@ -1,6 +1,7 @@
+import pandas
 import plotly.graph_objects as go
 import plotly.io as pio
-import pandas
+import plotly.express as px
 
 # Read csv file
 df = pandas.read_csv('/content/user_data.csv')
@@ -8,46 +9,46 @@ ds = pandas.DataFrame(df)
 print(df)
 
 #define the funnel stages
-stages = ['homepage', 'product_page', 'cart', 'checkout', 'purchase']
+Funnel_Stages = ['homepage', 'product_page', 'cart', 'checkout', 'purchase']
 
 #calculate the number of users and conversions for each stage
-users = []
-conversions = []
+no_of_users = []
+no_of_conversions = []
 
-for i in stages:
-    curr_user = df[df['stage'] == i] # It creates a new DataFrame curr_users that only contains rows where the value in the 'stage' column matches the current stage in the loop
-    users.append(len(curr_user))
-    conversions.append(curr_user['conversion'].sum()) #sum() gives the the count of only True values in conversion,hence no of conversions can be calculated
+for stages in Funnel_Stages:
+    stage_users = df[df['stage'] == stages ]  # It creates a new DataFrame stage_users that only contains rows where the value in the 'stage' column matches the current stage in the loop
+    no_of_users.append(len(stage_users))
+    no_of_conversions.append(stage_users['conversion'].sum()) # sum() gives the the count of only True values in conversion,hence no of conversions can be calculated
 
-print("No of users",users)
-print("No of Conversions",conversions)
+print("Number of Users : ",no_of_users)
+print("Number of Conversions : ",no_of_conversions)
 
 #create a funnel chart
 fig = go.Figure(go.Funnel(
-    y=stages,
-    x=users,
+    y=Funnel_Stages,
+    x=no_of_users,
     textposition='inside',
     name='Users',
-    marker = {"color": ["pink", "lightgreen", "violet", "teal", "black"]}
-))
+    marker={'color':'lightblue'}))
 
-#Customize the appearance of each trace using any relevant arguments
+pio.templates.default = "plotly_white"
+
+#Adding the trace using any relevant arguments
 fig.add_trace(go.Funnel(
-    y=stages,
-    x=conversions,
+    y=Funnel_Stages,
+    x=no_of_conversions,
     textposition='inside',
     textinfo='value',
     name='Conversions',
-    marker = {"color": ["turquoise", "yellow", "green", "blue", "orange"]}
-))
+    marker={'color':'blue'}))
 
+#Customize the appearance of the chart
 fig.update_layout(
     title_text='User Funnel Analysis',
     title_x=0.5,
-    template='plotly_white',
     height=800,
-    width=1200,
-    funnelmode='stack'
-)
+    width=1300,
+    funnelmode='stack',
+    legend=dict(orientation='h',y=0.001,x=0.43))
 
 fig.show()
